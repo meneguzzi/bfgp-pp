7b42ca944c177c12e91e38eac0c82787afb8fe6a
Felipe Meneguzzi <felipe.meneguzzi@gmail.com>
2024-06-21 19:24:48 -0600

    Minor changes to build under MacOS

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,3 +1,6 @@
 echo 'Compiling the main project...'
-g++ -Wall -O3 -std=c++17  src/main.cpp -o main.bin
-
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    g++ -Wall -O3 -std=c++17  src/main.cpp -o main.bin
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    /opt/homebrew/bin/g++-14 -Wall -O3 -std=c++17  src/main.cpp -o main.bin
+fi
